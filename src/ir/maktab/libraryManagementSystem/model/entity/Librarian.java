1766dd0638fbc7b54879b6da0987a1aa8d0cb6d8
Reihaneh Mahjour <rihaneh763@gmail.com>
2022-09-15 11:17:11 +0430

    add Librarian class to the model.entity package

---
+++ src/ir/maktab/libraryManagementSystem/model/entity/Librarian.java
@@ -0,0 +1,65 @@
+package ir.maktab.libraryManagementSystem.model.entity;
+
+import java.util.Objects;
+
+public class Librarian {
+    private int id;
+    private String name;
+    private String password;
+    private String searchString;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSearchString() {
+        return searchString;
+    }
+
+    public void setSearchString(String searchString) {
+        this.searchString = searchString;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Librarian librarian = (Librarian) o;
+        return id == librarian.id && Objects.equals(name, librarian.name) && Objects.equals(password, librarian.password) && Objects.equals(searchString, librarian.searchString);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, password, searchString);
+    }
+
+    @Override
+    public String toString() {
+        return "Librarian{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", searchString='" + searchString + '\'' +
+                '}';
+    }
+}
