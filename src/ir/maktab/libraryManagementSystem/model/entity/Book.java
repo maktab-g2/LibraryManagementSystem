cd9412777c64827191ffcba7470a7311d8eb0d5f
Reihaneh Mahjour <rihaneh763@gmail.com>
2022-09-15 11:12:59 +0430

    add Book class to the model.entity package

---
+++ src/ir/maktab/libraryManagementSystem/model/entity/Book.java
@@ -0,0 +1,65 @@
+package ir.maktab.libraryManagementSystem.model.entity;
+
+import java.util.Objects;
+
+public class Book {
+    private long isbn;
+    private String title;
+    private String Author;
+    private String publication;
+
+    public long getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(long isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+
+    public String getPublication() {
+        return publication;
+    }
+
+    public void setPublication(String publication) {
+        this.publication = publication;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return isbn == book.isbn && Objects.equals(title, book.title) && Objects.equals(Author, book.Author) && Objects.equals(publication, book.publication);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isbn, title, Author, publication);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isbn=" + isbn +
+                ", title='" + title + '\'' +
+                ", Author='" + Author + '\'' +
+                ", publication='" + publication + '\'' +
+                '}';
+    }
+}
